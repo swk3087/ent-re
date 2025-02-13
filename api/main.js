065630312ac0a69f957a1fca82cf2f821bf5c229
swk <swk1072@gmail.com>
2025-02-13 14:25:53 +0900

    updated

--- api/main.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Content-Type", "application/javascript");
-    res.status(200).send(`
-        console.log("loaded!");
-    `);
-}
