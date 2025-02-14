da21824046fe3c3bb92167ad0d64e2a114569956
swk <swk1072@gmail.com>
2025-02-14 16:26:37 +0900

    updated

--- main.js
+++ main.js
@@ -359,7 +359,7 @@ const LibraryCreator = {
       }
     }
 }
-const blocks = $.get('https://github.com/swk3087/ent-re/blocks.js');
+const blocks = $.get('https://swk3087.github.io/ent-re/blocks.js');
 document.title = "Entry_Strong_Block";
 LibraryCreator.start(blocks, 'API', '추가블럭')
 alert('블럭 로딩완료');
